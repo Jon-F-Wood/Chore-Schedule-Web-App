eac68fe06a0a0a2b35f70b79a8569a6fde40e5ee
Jon-F-Wood <woodj14123@gmail.com>
2017-01-17 01:58:45 -0600

    Finally completed side menu functionality

---
+++ js/main.js
@@ -0,0 +1,35 @@
+//sideNav Constructor
+function sideNav(options) {
+	//Initialise sideNav
+	var mask = document.getElementById("c-mask");
+	this.menu = document.getElementById(options.side);
+	this.btn = document.getElementById(options.btnId);
+	//Open sideNav when corresponding button is clicked
+	this.btn.addEventListener('click', function() {		
+		this.menu.classList.add('is-active');
+		mask.classList.add('is-active');
+	}.bind(this));
+	//Close sideNav when mask is clicked
+	mask.addEventListener('click', function() {		
+		this.menu.classList.remove('is-active');
+		mask.classList.remove('is-active');
+	}.bind(this));
+}
+
+//Create leftMenu
+var slideLeft = new sideNav({
+	side: 'leftMenu',
+	btnId: 'menu'
+});
+
+//Create rightMenu
+var slideRight = new sideNav({
+	side: 'rightMenu',
+	btnId: 'noteNav'
+});
+
+
+
+
+
+

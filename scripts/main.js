8d8a7fbcee304c6e628e53ecdb737cd4a731d164
Jon-F-Wood <woodj14123@gmail.com>
2017-03-25 20:11:33 -0500

    moving around files for server organization

--- scripts/main.js
+++ scripts/main.js
@@ -36,13 +36,13 @@ $( document ).ready(function() {
 		}
 	};
 	if (window.history && window.history.pushState) {
-	    window.history.pushState('forward', null, './#forward');
+        window.history.pushState('', null, './');
+        $(window).on('popstate', function() {
+            alert('Back button was pressed.');
+            document.location.href = '#';
 
-	    $(window).on('popstate', function() {
-	      alert('Back button was pressed.');
-	    });
-
-	  }
+        });
+    }
 	//Initialise sideNav
 	var mask = $("#mask");
 	var menu = $("#leftMenu");
